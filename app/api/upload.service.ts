5fe03dab85ab07f462a8e9e199bdfec1f8b99afa
Antonyo Andrei <antonyoandrei7@gmail.com>
2023-12-22 14:38:26 +0100

    sameorigin

--- app/api/upload.service.ts
+++ app/api/upload.service.ts
@@ -10,7 +10,7 @@ export const uploadRequest = async (
     const response: Response = await fetch(`${url}/movie/upload/image`, {
       method: "POST",
       body: formData,
-      mode: 'navigate'
+      mode: 'same-origin'
     });
 
     const data = await response.json();
