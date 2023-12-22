3bb0bd51ef63e3e5f46a0adbabcecf7451e9df4a
Antonyo Andrei <antonyoandrei7@gmail.com>
2023-12-22 13:24:41 +0100

    cors

--- app/api/upload.service.ts
+++ app/api/upload.service.ts
@@ -10,7 +10,7 @@ export const uploadRequest = async (
     const response: Response = await fetch(`${url}/movie/upload/image`, {
       method: "POST",
       body: formData,
-      mode: 'no-cors'
+      mode: 'cors'
     });
 
     const data = await response.json();
