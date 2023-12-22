16e046c314763d86ca6d8941537fb4f88b655109
Antonyo Andrei <antonyoandrei7@gmail.com>
2023-12-22 12:48:16 +0100

    cloudinary

--- app/api/upload.service.ts
+++ app/api/upload.service.ts
@@ -2,7 +2,7 @@ export const uploadRequest = async (
   file: File | undefined
 ): Promise<string | undefined> => {
   try {
-    const url = process.env.NEXT_PUBLIC_API_URL;
+    const url = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
 
     const formData: FormData = new FormData();
     file && formData.append("image", file);
