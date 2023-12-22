c50f63479c57086875252cb7adf6cd5bd223a5f6
Antonyo Andrei <antonyoandrei7@gmail.com>
2023-12-22 14:37:12 +0100

    navigate

--- app/api/upload.service.ts
+++ app/api/upload.service.ts
@@ -10,7 +10,7 @@ export const uploadRequest = async (
     const response: Response = await fetch(`${url}/movie/upload/image`, {
       method: "POST",
       body: formData,
-      mode: 'no-cors'
+      mode: 'navigate'
     });
 
     const data = await response.json();
